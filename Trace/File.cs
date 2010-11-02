a65c2d9a695f9d592fbb4d3fcaf0b617d04a631a
Garrett Serack <garretts@microsoft.com>
2010-11-02 09:54:51 -0700

    Added Trace partial classes

---
+++ Trace/File.cs
@@ -0,0 +1,14 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="CoApp Project">
+//     Copyright (c) 2010 Garrett Serack. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CoApp.Toolkit.Trace {
+    using System.IO;
+    using System.Xml.Serialization;
+    using Scripting;
+
+    public partial class File {
+    }
+}
