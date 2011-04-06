726d0a22f16ae776e2fef90a2ca43713137b8b76
Rafael Rivera <rafael@withinwindows.com>
2011-04-06 11:03:05 -0700

    Missing file.

---
+++ Win32/Ntdll.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="CoApp Project">
+//     Copyright (c) 2011 Garrett Serack . All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CoApp.Toolkit.Win32
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    public static class Ntdll
+    {
+        [DllImport("ntdll.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern UInt32 RtlAcquirePrivilege(ref UInt32 Privilege, UInt32 NumPriv, UInt32 Flags, ref IntPtr ReturnedState);
+
+        [DllImport("ntdll.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern UInt32 RtlReleasePrivilege(IntPtr ReturnedState);
+    }
+}
