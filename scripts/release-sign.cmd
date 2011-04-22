a9aa254623a7ca438f0253c07cf360bc25aa991a
Garrett Serack <garretts@microsoft.com>
2011-04-22 13:02:08 -0700

    merge trace into master build

--- scripts/release-sign.cmd
+++ scripts/release-sign.cmd
@@ -2,3 +2,4 @@
 :release-sign.cmd
 cd %~dp0
 start /wait coapp-simplesigner ..\..\output\any\release\bin\*.exe ..\..\output\any\release\bin\*.dll
+start /wait coapp-simplesigner --sign-only "..\..\output\any\release\bin\detour*.dll"
