0951bf952ab0aba4a01bb95f913cff62e222b5bd
Garrett Serack <garretts@microsoft.com>
2011-05-16 15:06:15 -0700

    Changed Property*Exception classes to EndUserExceptions

---
+++ Exceptions/EndUserParseException.cs
@@ -0,0 +1,19 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="CoApp Project">
+//     Copyright (c) 2010 Garrett Serack . All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CoApp.Toolkit.Exceptions {
+    using System;
+    using Extensions;
+    using Scripting.Utility;
+
+    public class EndUserParseException : Exception {
+        public Token Token;
+        public EndUserParseException(Token token,string filename, string errorcode, string message, params object[] parameters)
+            : base("{0}({1},{2}):{3}:{4}".format(filename, token.Row,token.Column, errorcode ,message.format(parameters))) {
+            Token = token;
+        }
+    }
+}
