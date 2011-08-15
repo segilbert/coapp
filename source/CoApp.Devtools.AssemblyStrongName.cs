b6178d2bf161b6d4363df62a624ef8efb6ce5461
Garrett Serack <garretts@microsoft.com>
2011-08-15 14:13:03 -0700

    it's compilining

--- source/CoApp.Devtools.AssemblyStrongName.cs
+++ source/CoApp.Devtools.AssemblyStrongName.cs
@@ -16,8 +16,8 @@ using System.Runtime.InteropServices;
 #pragma warning disable 1699
 
 #if SIGN_ASSEMBLY || TEST_SIGN_ASSEMBLY
-[assembly: AssemblyVersion("1.1.2.152")] //SIGNED VERSION
-[assembly: AssemblyFileVersion("1.1.2.152")] //SIGNED VERSION
+[assembly: AssemblyVersion("1.1.2.212")] //SIGNED VERSION
+[assembly: AssemblyFileVersion("1.1.2.212")] //SIGNED VERSION
 
 #if SIGN_ASSEMBLY 
     [assembly: AssemblyKeyFileAttribute(@"..\signing\coapp-release-public-key.snk")]
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 
 #else
 
-[assembly: AssemblyVersion("1.1.1.152")] //UNSIGNED VERSION
-[assembly: AssemblyFileVersion("1.1.1.152")] //UNSIGNED VERSION
+[assembly: AssemblyVersion("1.1.1.212")] //UNSIGNED VERSION
+[assembly: AssemblyFileVersion("1.1.1.212")] //UNSIGNED VERSION
 
 #endif
 
@@ -48,6 +48,66 @@ internal class AssemblyBugtrackerAttribute : Attribute {
         return TrackerUrl;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
