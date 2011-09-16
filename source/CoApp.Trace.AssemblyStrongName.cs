e4390b6dab10e8e8f66527b34a6789031bc3a87f
Garrett Serack <garretts@microsoft.com>
2011-09-16 16:08:07 -0700

    Added rehash, new bootstrapper, bootstrap UI, resources, new simplesigner

--- source/CoApp.Trace.AssemblyStrongName.cs
+++ source/CoApp.Trace.AssemblyStrongName.cs
@@ -16,8 +16,8 @@ using System.Runtime.InteropServices;
 #pragma warning disable 1699
 
 #if SIGN_ASSEMBLY || TEST_SIGN_ASSEMBLY
-[assembly: AssemblyVersion("1.1.2.7")] //SIGNED VERSION
-[assembly: AssemblyFileVersion("1.1.2.7")] //SIGNED VERSION
+[assembly: AssemblyVersion("1.1.2.9")] //SIGNED VERSION
+[assembly: AssemblyFileVersion("1.1.2.9")] //SIGNED VERSION
 
 #if SIGN_ASSEMBLY 
     [assembly: AssemblyKeyFileAttribute(@"..\signing\coapp-release-public-key.snk")]
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 
 #else
 
-[assembly: AssemblyVersion("1.1.1.7")] //UNSIGNED VERSION
-[assembly: AssemblyFileVersion("1.1.1.7")] //UNSIGNED VERSION
+[assembly: AssemblyVersion("1.1.1.9")] //UNSIGNED VERSION
+[assembly: AssemblyFileVersion("1.1.1.9")] //UNSIGNED VERSION
 
 #endif
 
@@ -48,6 +48,8 @@ internal class AssemblyBugtrackerAttribute : Attribute {
         return TrackerUrl;
     }
 }
+
+
 
 
 
