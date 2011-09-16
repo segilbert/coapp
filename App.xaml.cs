4d22c5d260a0ecab998c3d948cf021c7f81a6688
Garrett Serack <garretts@microsoft.com>
2011-09-16 16:15:59 -0700

    Initial Import

---
+++ App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace BootstrapperUI {
+    using System.Drawing;
+    using System.IO;
+    using System.Reflection;
+    using System.Runtime.InteropServices;
+    using System.Text;
+    using System.Windows.Media.Imaging;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace CoApp.Bootstrapper {
+    using System.Drawing;
+    using System.Windows;
+    using System.Windows.Input;
+    using System.Windows.Interop;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using BootstrapperUI;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+
+            sp1.Background.SetValue(ImageBrush.ImageSourceProperty, NativeResources.GetBitmapImage(1201));
+            logoImage.SetValue(System.Windows.Controls.Image.SourceProperty, NativeResources.GetBitmapImage(1202));
+        }
+
+        private void HeaderMouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            DragMove();
+        }
+
+        private void CloseBtnClick(object sender, RoutedEventArgs e) {
+            // stop the download/install...
+            Application.Current.Shutdown();
+        }
+    }
+}
