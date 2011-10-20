b5ced82a2ca92fd42e9dd5dfa7a4f276b446e1b3
Garrett Serack <garretts@microsoft.com>
2011-10-20 09:45:24 -0700

    toolkit updated

--- source/CoApp.Devtools.AssemblyStrongName.cs
+++ source/CoApp.Devtools.AssemblyStrongName.cs
@@ -16,8 +16,8 @@ using System.Runtime.InteropServices;
 // we publish it, so now we will have just one set of  Version  lines, and no strong 
 // naming until publishing.
 
-[assembly: AssemblyVersion("1.1.2.1101")]
-[assembly: AssemblyFileVersion("1.1.2.1101")]
+[assembly: AssemblyVersion("1.1.2.1102")]
+[assembly: AssemblyFileVersion("1.1.2.1102")]
 
 [AttributeUsage(AttributeTargets.Assembly, Inherited = false)]
 internal class AssemblyBugtrackerAttribute : Attribute {
--- source/CoApp.Toolkit.AssemblyStrongName.cs
+++ source/CoApp.Toolkit.AssemblyStrongName.cs
@@ -16,8 +16,8 @@ using System.Runtime.InteropServices;
 // we publish it, so now we will have just one set of  Version  lines, and no strong 
 // naming until publishing.
 
-[assembly: AssemblyVersion("1.1.2.1137")]
-[assembly: AssemblyFileVersion("1.1.2.1137")]
+[assembly: AssemblyVersion("1.1.2.1138")]
+[assembly: AssemblyFileVersion("1.1.2.1138")]
 
 [AttributeUsage(AttributeTargets.Assembly, Inherited = false)]
 internal class AssemblyBugtrackerAttribute : Attribute {
