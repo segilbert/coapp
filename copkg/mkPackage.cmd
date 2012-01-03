dad96ce7446ec5b7c4ce71f4a48a59d89e6a73a2
Garrett Serack <garretts@microsoft.com>
2012-01-03 15:40:41 -0800

    tweaked mkPackage script

--- copkg/mkPackage.cmd
+++ copkg/mkPackage.cmd
@@ -1,4 +1,16 @@
 @echo off
+cd %~dp0
+
 erase *.msi 
 erase *.wixpdb
-autopackage template.autopkg outercurve.autopkg coapp.toolkit.autopkg 
+
+echo Killing old service.
+net stop "CoApp Package Installer Service" > NUL
+pskill coapp.service > NUL
+
+start ..\output\any\Release\bin\coapp.service.exe --interactive
+
+..\ext\tools\autopackage outercurve.autopkg coapp.toolkit.autopkg  || goto EOF:
+
+for %%v  in (*.msi) do curl -T  %%v http://coapp.org/upload/ || goto EOF:
+echo "Uploaded to repository"
